64dc4626f257602a66c2d9586712dc851d5bcf5c
Andrew Macdonald <andrew.dot.macdonald@gmail.com>
2015-11-28 18:33:54 -0500

    Added default EgoInterface Component

---
+++ EgoInterface.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EgoInterface : MonoBehaviour
+{
+	void Start()
+    {
+        // Add Systems here:
+        //EgoSystems.Add( new ExampleSystem() );
+
+        EgoSystems.Start();
+        EgoEvents.Invoke();
+	}
+	
+	void Update()
+    {
+        EgoSystems.Update();
+        EgoEvents.Invoke();
+    }
+
+    void FixedUpdate()
+    {
+        EgoSystems.FixedUpdate();
+    }
+}
