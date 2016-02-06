f164cc2c2e94e3015ba13295bf957108f9be749c
Andrew Macdonald <andrew.dot.macdonald@gmail.com>
2016-02-06 14:47:35 -0500

    Remove Systems from upcoming EgoCS example project

--- EgoInterface.cs
+++ EgoInterface.cs
@@ -4,31 +4,20 @@ using System.Collections;
 public class EgoInterface : MonoBehaviour
 {
 	void Start()
-    {
-        // Add Systems here:
-        EgoSystems.Add( new BallSystem() );
-        EgoSystems.Add( new BrickSystem() );
-        EgoSystems.Add( new BrickInstantiationSystem() );
-        EgoSystems.Add( new PaddleSystem() );
-
-        // Win Conditions
-        EgoSystems.Add( new ScoreSystem() );
-        EgoSystems.Add( new GameEndSystem() );
-
-        // UI
-        EgoSystems.Add( new PlayAgainButtonSystem() );
-        EgoSystems.Add( new UISystem() );
-
-        EgoSystems.Start(); 
+	{
+		// Add Systems here:
+		//EgoSystems.Add( new ExampleSystem() );
+		
+		EgoSystems.Start();
 	}
 	
 	void Update()
-    {
-        EgoSystems.Update();
-    }
+	{
+		EgoSystems.Update();
+	}
 
-    void FixedUpdate()
-    {
-        EgoSystems.FixedUpdate();
-    }
+	void FixedUpdate()
+	{
+		EgoSystems.FixedUpdate();
+	}
 }
