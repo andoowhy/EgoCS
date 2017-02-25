2892245dd1e14c3c572d5104d47b5c20662291c0
Andrew Macdonald <andrew.dot.macdonald@gmail.com>
2017-02-25 14:02:49 -0500

    Renamed "AddToFirstEvents" and "AddToLastEvents" to "AddFront" and "AddEnd", respectively.

--- EgoEvents.cs
+++ EgoEvents.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 
 public static class EgoEvents
 {
-    static List<Type> _firstEvents = new List<Type>();
-    static List<Type> _lastEvents = new List<Type>();
+	static List<Type> _firstEvents = new List<Type>();
+	static List<Type> _lastEvents = new List<Type>();
 
-    static List<Type> _userOrderedFirstEvents = new List<Type>();
-    static List<Type> _userOrderedLastEvents = new List<Type>();
+	static List<Type> _userOrderedFirstEvents = new List<Type>();
+	static List<Type> _userOrderedLastEvents = new List<Type>();
 
-    static HashSet<Type> _unorderedEvents = new HashSet<Type>();
-    public static HashSet<Type> unorderedEvents
-    {
-        get { return _unorderedEvents; }
-    }
+	static HashSet<Type> _unorderedEvents = new HashSet<Type>();
+	public static HashSet<Type> unorderedEvents
+	{
+		get { return _unorderedEvents; }
+	}
 
-    static Dictionary<Type, Action> _invokeLookup = new Dictionary<Type, Action>();
-    public static Dictionary<Type, Action> invokeLookup
-    {
-        get { return _invokeLookup; }
-    }
+	static Dictionary<Type, Action> _invokeLookup = new Dictionary<Type, Action>();
+	public static Dictionary<Type, Action> invokeLookup
+	{
+		get { return _invokeLookup; }
+	}
 
-    public static void Start()
-    {
+	public static void Start()
+	{
 		foreach( var assembly in AppDomain.CurrentDomain.GetAssemblies() )
 		{
 			foreach( var type in assembly.GetTypes() )
@@ -39,28 +39,28 @@ public static class EgoEvents
 		_firstEvents.Add( typeof( AddedGameObject ) );
 		_unorderedEvents.Remove( typeof( AddedGameObject ) );
 
-        _lastEvents.Add( typeof( DestroyedGameObject ) );
+		_lastEvents.Add( typeof( DestroyedGameObject ) );
 		_unorderedEvents.Remove( typeof( DestroyedGameObject ) );
-		
-        ComponentIDs.componentTypes.ForEach( componentType =>
-        {
-            MakeComponentEventInvoke( componentType, typeof( AddedComponent<> ), ref _firstEvents );
-            MakeComponentEventInvoke( componentType, typeof( DestroyedComponent<> ), ref _lastEvents );
-        } );
+
+		ComponentIDs.componentTypes.ForEach( componentType =>
+		{
+			MakeComponentEventInvoke( componentType, typeof( AddedComponent<> ), ref _firstEvents );
+			MakeComponentEventInvoke( componentType, typeof( DestroyedComponent<> ), ref _lastEvents );
+		} );
 
 		_unorderedEvents.ExceptWith( _firstEvents );
 		_unorderedEvents.ExceptWith( _userOrderedFirstEvents );
 		_unorderedEvents.ExceptWith( _userOrderedLastEvents );
 		_unorderedEvents.ExceptWith( _lastEvents );
-    }
+	}
 
-	public static void AddToFirstEvents<E>() where E : EgoEvent
+	public static void AddFront<E>() where E : EgoEvent
 	{
 		var e = typeof( E );
 		_userOrderedFirstEvents.Add( e );
 	}
 
-	public static void AddToLastEvents<E>() where E : EgoEvent
+	public static void AddEnd<E>() where E : EgoEvent
 	{
 		var e = typeof( E );
 		_userOrderedLastEvents.Add( e );
@@ -76,7 +76,7 @@ public static class EgoEvents
 	{
 		var componentEventType = genericComponentEventType.MakeGenericType( eventType );
 		var fullEventType = typeof( EgoEvents<> ).MakeGenericType( componentEventType );
-		fullEventType.GetMethod("Init", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic ).Invoke( null, null );
+		fullEventType.GetMethod( "Init", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic ).Invoke( null, null );
 		eventList.Add( componentEventType );
 	}
 
@@ -91,10 +91,10 @@ public static class EgoEvents
 }
 
 public static class EgoEvents<E>
-    where E : EgoEvent
+	where E : EgoEvent
 {
-    static List<E> _events = new List<E>();
-    static List<Action<E>> _handlers = new List<Action<E>>();
+	static List<E> _events = new List<E>();
+	static List<Action<E>> _handlers = new List<Action<E>>();
 
 	static EgoEvents()
 	{
@@ -109,43 +109,43 @@ public static class EgoEvents<E>
 		EgoEvents.unorderedEvents.Add( e );
 	}
 
-    static void Invoke()
-    {
-        var length = _events.Count;
-        for( int i = 0; i < length; i++ )
-        {
-            foreach( var handler in _handlers )
-            {
+	static void Invoke()
+	{
+		var length = _events.Count;
+		for( int i = 0; i < length; i++ )
+		{
+			foreach( var handler in _handlers )
+			{
 #if UNITY_EDITOR
-                EgoSystem system = null;
-                if( handler.Target is EgoSystem )
-                {
-                    system = handler.Target as EgoSystem;
-                }
-                else if( handler.Target is EgoConstraint )
-                {
-                    system = ( handler.Target as EgoConstraint ).system;
-                }
+				EgoSystem system = null;
+				if( handler.Target is EgoSystem )
+				{
+					system = handler.Target as EgoSystem;
+				}
+				else if( handler.Target is EgoConstraint )
+				{
+					system = ( handler.Target as EgoConstraint ).system;
+				}
 
-                if( system != null && system.enabled )
-                {
-                    handler( _events[i] );
-                }
+				if( system != null && system.enabled )
+				{
+					handler( _events[ i ] );
+				}
 #else
                 handler( _events[i] );
 #endif
-            }
-        }
-        _events.RemoveRange( 0, length );
-    }
+			}
+		}
+		_events.RemoveRange( 0, length );
+	}
 
-    public static void AddHandler( Action<E> handler )
-    {
-        _handlers.Add( handler );
-    }
+	public static void AddHandler( Action<E> handler )
+	{
+		_handlers.Add( handler );
+	}
 
-    public static void AddEvent( E e )
-    {
-        _events.Add( e );
-    }
+	public static void AddEvent( E e )
+	{
+		_events.Add( e );
+	}
 }
